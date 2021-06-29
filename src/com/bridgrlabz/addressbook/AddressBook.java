18880766b31e8c4d57ced30e9d1a2e83d056d6f3
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-29 17:38:53 +0530

    added the functionality to edit a person details using their name

--- src/com/bridgrlabz/addressbook/AddressBook.java
+++ src/com/bridgrlabz/addressbook/AddressBook.java
@@ -12,41 +12,87 @@ public class AddressBook
 	{
 		int numberOfContacts;
 		System.out.println("Enter number of required contacts :");
-
-		try {
-			numberOfContacts = scanner.nextInt();
+		numberOfContacts = scanner.nextInt();
 			
-			Contacts arrayOfContacts[] = new Contacts[numberOfContacts];
-			for (int index = 0; index < numberOfContacts; index++)
-			{
-				addressBook = new Contacts();
-				System.out.println("Enter First Name : ");
-				addressBook.setFirst(scanner.next());
-				System.out.println("Enter Last Name :");
-				addressBook.setLast(scanner.next());
-				System.out.println("Enter address :");
-				addressBook.setAddress(scanner.next()); 
-				System.out.println("Enter City");
-				addressBook.setCity(scanner.next());
-				System.out.println("Enter State");
-				addressBook.setState(scanner.next());
-				System.out.println("Enter Zip :");
-				addressBook.setZip(scanner.nextInt());
-				System.out.println("Enter Phone Number :");
-				addressBook.setPhoneNumber(scanner.nextInt());
-				System.out.println("Enter email :"); addressBook.setEmail(scanner.next());
+		Contacts arrayOfContacts[] = new Contacts[numberOfContacts];
+		for (int index = 0; index < numberOfContacts; index++)
+		{
+			addressBook = new Contacts();
+			System.out.println("Enter First Name : ");
+			addressBook.setFirst(scanner.next());
+			System.out.println("Enter Last Name :");
+			addressBook.setLast(scanner.next());
+			System.out.println("Enter address :");
+			addressBook.setAddress(scanner.next()); 
+			System.out.println("Enter City");
+			addressBook.setCity(scanner.next());
+			System.out.println("Enter State");
+			addressBook.setState(scanner.next());
+			System.out.println("Enter Zip :");
+			addressBook.setZip(scanner.nextInt());
+			System.out.println("Enter Phone Number :");
+			addressBook.setPhoneNumber(scanner.nextInt());
+			System.out.println("Enter email :"); 
+			addressBook.setEmail(scanner.next());
 				 
-				arrayOfContacts[index] = addressBook;				
-			}
-			
-				for (Contacts addContact : arrayOfContacts) 
-				{
-				System.out.println(addContact); 
-				}
-				
-		} catch (Exception e) {
-			e.printStackTrace();
+			arrayOfContacts[index] = addressBook;				
 		}
-
+			
+		// to print all the contacts
+		for (Contacts addContact : arrayOfContacts) 
+		{
+			System.out.println(addContact); 
+		}
+		
+		
+		//To Edit the persons contact
+		System.out.println("Enter the name to check for details :");
+		String checkValue = scanner.next();
+		for(int i =0 ;i<arrayOfContacts.length;i++)
+		{
+			if(arrayOfContacts[i].getFirst().equals(checkValue))
+			{
+				System.out.println("Record Found");
+				addressBook = new Contacts();
+				addressBook = arrayOfContacts[i];
+				System.out.println(arrayOfContacts[i]);
+				System.out.println("Enter '1' to change First Name \nEnter '2' to change Last Name \nEnter '3' to change Address \nEnter '4' to change City  \nEnter '5' to change state ");
+				int number = scanner.nextInt();
+				switch (number) {
+				case 1: {
+					System.out.println("Enter the new first Name :");
+					addressBook.setFirst(scanner.next());
+					System.out.println("New Record :"+arrayOfContacts[i]);
+					break;
+				}
+				case 2: {
+					System.out.println("Enter the new second Name :");
+					addressBook.setLast(scanner.next());
+					System.out.println("New Record :"+arrayOfContacts[i]);
+					break;
+				}
+				case 3: {
+					System.out.println("Enter the new Address :");
+					addressBook.setAddress(scanner.next());
+					System.out.println("New Record :"+arrayOfContacts[i]);
+					break;
+				}
+				case 4: {
+					System.out.println("Enter the new City :");
+					addressBook.setAddress(scanner.next());
+					System.out.println("New Record :"+arrayOfContacts[i]);
+					break;
+				}
+				case 5: {
+					System.out.println("Enter the new State :");
+					addressBook.setState(scanner.next());
+					System.out.println("New Record :"+arrayOfContacts[i]);
+					break;
+				}
+				default:
+					System.out.println("Invalid Numbers");
+				}
+			}
+		}
 	}
 }
