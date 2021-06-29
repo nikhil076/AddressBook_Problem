cc6a927106d86c2ac9d875dc5a674f4fee2d147d
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-29 23:48:38 +0530

    added funtionality to delete a contact

--- src/com/bridgrlabz/addressbook/AddressBook.java
+++ src/com/bridgrlabz/addressbook/AddressBook.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class AddressBook 
 {
 	static Contacts addressBook;
-
+	static Contacts arrayOfContacts[];
 	static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) 
@@ -14,7 +14,7 @@ public class AddressBook
 		System.out.println("Enter number of required contacts :");
 		numberOfContacts = scanner.nextInt();
 			
-		Contacts arrayOfContacts[] = new Contacts[numberOfContacts];
+		arrayOfContacts = new Contacts[numberOfContacts];
 		for (int index = 0; index < numberOfContacts; index++)
 		{
 			addressBook = new Contacts();
@@ -24,7 +24,7 @@ public class AddressBook
 			addressBook.setLast(scanner.next());
 			System.out.println("Enter address :");
 			addressBook.setAddress(scanner.next()); 
-			System.out.println("Enter City");
+			System.out.println("Enter City"); 
 			addressBook.setCity(scanner.next());
 			System.out.println("Enter State");
 			addressBook.setState(scanner.next());
@@ -32,22 +32,34 @@ public class AddressBook
 			addressBook.setZip(scanner.nextInt());
 			System.out.println("Enter Phone Number :");
 			addressBook.setPhoneNumber(scanner.nextInt());
-			System.out.println("Enter email :"); 
+			System.out.println("Enter email :");
 			addressBook.setEmail(scanner.next());
-				 
+			 
 			arrayOfContacts[index] = addressBook;				
 		}
-			
-		// to print all the contacts
+		printAllContacts();
+		System.out.println("Enter the name to edit details :");
+		String checkName = scanner.next();
+		Contacts contacts[] = new Contacts[numberOfContacts];
+		String name[]= new String[numberOfContacts];
+		System.out.println("Enter the name to Delete thaaat contact :");
+		String checkValue = scanner.next();
+		editContacts(checkName);
+		deleteUser(checkValue,name);
+		
+	}
+	
+	public static void printAllContacts() 
+	{
 		for (Contacts addContact : arrayOfContacts) 
 		{
 			System.out.println(addContact); 
 		}
-		
-		
-		//To Edit the persons contact
+	}
+	
+	public static void editContacts(String checkValue)
+	{
 		System.out.println("Enter the name to check for details :");
-		String checkValue = scanner.next();
 		for(int i =0 ;i<arrayOfContacts.length;i++)
 		{
 			if(arrayOfContacts[i].getFirst().equals(checkValue))
@@ -93,6 +105,43 @@ public class AddressBook
 					System.out.println("Invalid Numbers");
 				}
 			}
+		}
+	}
+	
+	public static void deleteUser(String name,String[] user)
+	{
+		System.out.println("Enter the name to check for details :");
+		for(int i =0 ;i<arrayOfContacts.length;i++)
+		{
+			
+			if(arrayOfContacts[i].getFirst().equals(name))
+			{
+				for(int j=0;j<user.length;j++)
+				{
+					if(user[j]!=null) {
+						continue;
+					}
+					else {
+						user[j]=name;
+					}
+				}
+			}
+		}		
+		for(int i =0;i<arrayOfContacts.length;i++)
+		{
+			int refactor=0;
+			for(int j =0;j<user.length;j++)
+			{
+				if(arrayOfContacts[i].getFirst().equals(user[j]))
+				{
+					refactor=1;
+					continue;
+				}
+			}
+			if(refactor==0)
+			{
+				System.out.println(arrayOfContacts[i]);
+			}	
 		}
 	}
 }
